f6177bc148f3aad830cea615cace8e561f113627
Nieuwlaar <nieuwlaar@gmail.com>
2025-03-27 21:43:53 +0100

    added or

--- src/components/RdwNiscyDialog.vue
+++ src/components/RdwNiscyDialog.vue
@@ -47,12 +47,12 @@
           <!-- EUDI Link Section -->
           <div class="space-y-2">
             <!-- Divider with "or" text -->
-            <div class="relative my-4">
+            <div v-if="credentialOfferUri" class="relative my-4">
               <div class="absolute inset-0 flex items-center">
                 <div class="w-full border-t border-gray-300"></div>
               </div>
               <div class="relative flex justify-center text-sm">
-                <span class="px-2 bg-white text-gray-500">or</span>
+                <span class="px-2 bg-gray-100 text-gray-500">or</span>
               </div>
             </div>
 
